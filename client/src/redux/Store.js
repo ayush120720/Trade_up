84946a329e313a4bb74d2adb03b9b37542669a56
TanyaJhaveri <tanyajhaveri2002@gmail.com>
2025-10-12 19:41:41 +0530

    Update Store.js

--- client/src/redux/Store.js
+++ client/src/redux/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import globalReducer from './DarkModeSlice';
 import userReducer from './UserSlice';
 
 export const Store = configureStore({
   reducer: {
+    global: globalReducer,
     user: userReducer,
   },
 });
