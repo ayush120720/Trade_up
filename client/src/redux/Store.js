561c413cbad9a3cc50f9d6ace79bcbc0c9471b58
Niyatinagar <166269349+Niyatinagar@users.noreply.github.com>
2025-10-12 20:07:53 +0530

    Update Store.js

--- client/src/redux/Store.js
+++ client/src/redux/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import sidebarReducer from './SidebarSlice';
 import globalReducer from './DarkModeSlice';
 import userReducer from './UserSlice';
 
 export const Store = configureStore({
   reducer: {
+    sidebar: sidebarReducer,
     global: globalReducer,
     user: userReducer,
   },
