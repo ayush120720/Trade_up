f75a1925fd9d18dbd473f5c947111b99144ba2de
Niyatinagar <166269349+Niyatinagar@users.noreply.github.com>
2025-10-12 20:08:34 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./Theme";
 import { useSelector } from "react-redux";
 import LandingPage from "./pages/LandingPage/LandingPage";
-
+import Dashboard from "./pages/Dashboard/Dashboard";
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
@@ -17,13 +17,16 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Routes>
+        
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/" element={<LandingPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+
         </Routes>
       </ThemeProvider>
     </Router>
   );
 }
 
-export default App;
+export default App;
