f4b88cbd6b6ed7b304b3f5e4c08623c8002668f7
TanyaJhaveri <tanyajhaveri2002@gmail.com>
2025-10-12 19:42:51 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,29 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/Authentication/SignIn";
 import SignUp from "./pages/Authentication/SignUp";
-import { CssBaseline } from "@mui/material";
-
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./Theme";
+import { useSelector } from "react-redux";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 function App() {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
-
-    return (
-        <Router>
-                <CssBaseline />
-                <Routes>
-                    <Route path="/signin" element={<SignIn />} />
-                    <Route path="/signup" element={<SignUp />} />
-                </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/" element={<LandingPage />} />
+        </Routes>
+      </ThemeProvider>
+    </Router>
+  );
 }
 
-export default App;
+export default App;
