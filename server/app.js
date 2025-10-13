70aa4b8c3eca21352778974e50ecf4048cd7fa87
Virag Koradiya <viragworkmail@gmail.com>
2025-10-13 17:37:29 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,10 +7,15 @@ require("./config/db");
 const mongoose = require("mongoose");
 
 const authRoutes = require("./routes/authRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+
+const portfolioRoutes = require("./routes/portfolioRoutes");
 
 app.use(
     cors({
-        origin: ["http://localhost:3000"],
+        origin: [
+            "https://paper-lingo.vercel.app/", 
+            "http://localhost:3000"],
         methods: "GET,POST,PUT,DELETE,OPTIONS",
         allowedHeaders: ["Authorization", "Content-Type"],
         credentials: true,
@@ -22,6 +27,7 @@ app.options("*", cors()); // Handle preflight requests
 // Ensure CORS headers are set in all responses
 app.use((req, res, next) => {
     const allowedOrigins = [
+        "https://paper-lingo.vercel.app/",
         "http://localhost:3000",
     ];
     const origin = req.headers.origin;
@@ -40,14 +46,21 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
+//using ejs engine to render the change password form
+// app.set("view engine", "ejs");
+// app.use(express.urlencoded({ extended: false }));
+
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/transaction", transactionRoutes);
+app.use("/api/portfolio", portfolioRoutes);
+
 
 const port = process.env.PORT || 5000;
 
 // Start server
 app.listen(port, () => {
-    console.log(`Server started on port: ${port}`);
+    console.log(Server started on port: ${port});
 });
 
 module.exports = app;
