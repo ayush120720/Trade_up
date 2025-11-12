7a475ff5cb30e7bdfead2bcbcd2244f135d61678
Virag Koradiya <viragworkmail@gmail.com>
2025-11-12 15:17:11 +0530

    Add authentication routes for profile management

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,10 +2,23 @@ const express = require("express");
 const {
   register,
   login,
+  getProfile,
+  updateProfile,
+  deleteProfile,
+  forgotPassword,
+  resetPassword,
+  confirmPassword,
 } = require("../controllers/authController.js");
+const { protect } = require("../middleware/authMiddleware.js");
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.post("/forgotpassword", forgotPassword);
+router.get("/resetpassword/:id/:token", resetPassword);
+router.post("/confirmpassword/:id/:token", confirmPassword);
+router.get("/profile", protect, getProfile);
+router.put("/profile", protect, updateProfile);
+router.delete("/profile", protect, deleteProfile);
 
 module.exports = router;
