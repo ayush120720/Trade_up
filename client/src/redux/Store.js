c83545795daa9f6fe0ce15f8fe6c084eb96fe6d5
Niyatinagar <166269349+Niyatinagar@users.noreply.github.com>
2025-11-13 11:40:15 +0530

    Add mobileMenuReducer to the Redux store

--- client/src/redux/Store.js
+++ client/src/redux/Store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 import sidebarReducer from './SidebarSlice';
 import globalReducer from './DarkModeSlice';
 import userReducer from './UserSlice';
+import mobileMenuReducer from './MobileMenuSlice';
 
 export const Store = configureStore({
   reducer: {
     sidebar: sidebarReducer,
+    mobileMenu: mobileMenuReducer,
     global: globalReducer,
     user: userReducer,
   },
