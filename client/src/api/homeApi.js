2f6a132fb341ae666e7ed49916610f6e05da04d8
Aangi-Shah <shahaangi26@gmail.com>
2025-11-13 18:45:02 +0530

    Update homeApi.js
    Add fetchNews function to retrieve news data

--- client/src/api/homeApi.js
+++ client/src/api/homeApi.js
@@ -1,5 +1,15 @@
 import apiClient from "../services/apiClient";
 
+export const fetchNews = async () => {
+  try {
+    const response = await apiClient.get('/home/news');
+    return response.data;
+  } catch (error) {
+    console.error("Error while fetching news: ", error);
+    throw error;
+  }
+};
+
 export const fetchGainersAndLosers = async () => {
   try {
       const response = await apiClient.get('/home/top-gainers-losers');
