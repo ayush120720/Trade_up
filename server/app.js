f453a3dbb2cb7f75bc11e2f3c23319be1c8f1808
Harsh Parakh <85981083+harsh-parakh22@users.noreply.github.com>
2025-12-01 01:10:46 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,13 +13,14 @@ const portfolioRoutes = require("./routes/portfolioRoutes");
 const quizRoutes = require("./routes/quizRoutes");
 
 const allowedOrigins = [
-    "http://localhost:3000",
-    "https://trade-up-us.vercel.app/"   
+    "https://trade-up-us.vercel.app",   
+    "http://localhost:3000"             
 ];
 
 app.use(
     cors({
         origin: function (origin, callback) {
+ 
             if (!origin || allowedOrigins.includes(origin)) {
                 callback(null, true);
             } else {
@@ -27,9 +28,25 @@ app.use(
             }
         },
         methods: "GET,POST,PUT,DELETE,OPTIONS",
+        allowedHeaders: ["Authorization", "Content-Type"],
         credentials: true,
     })
 );
+
+app.options("*", cors());
+
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Authorization, Content-Type");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    next();
+});
 
 app.use(express.json());
 
@@ -40,8 +57,9 @@ app.use("/api/home", homeRoutes);
 app.use("/api/quiz", quizRoutes);
 
 const port = process.env.PORT || 8000;
+
 app.listen(port, () => {
-    console.log(`Server started on port: ${port}`);
+    console.log(`🚀 Server started on port: ${port}`);
 });
 
 module.exports = app;
